70e55a07db85b8e587eba862cd5a55e282fdb3df
Gguigre <gui.gremi@gmail.com>
2020-08-24 23:27:30 +0200

    Updates

---
+++ precache-manifest.05e81e13897ac825f62234426743750f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9094a0337bc611ca545f31a25b43236b",
+    "url": "/FirstAidGenerator/index.html"
+  },
+  {
+    "revision": "8623bb5d91cff6f1477a",
+    "url": "/FirstAidGenerator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a3404d23dc2f44b313e",
+    "url": "/FirstAidGenerator/static/js/2.60798068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstAidGenerator/static/js/2.60798068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8623bb5d91cff6f1477a",
+    "url": "/FirstAidGenerator/static/js/main.64f213db.chunk.js"
+  },
+  {
+    "revision": "01dccf915df5b3d80b07",
+    "url": "/FirstAidGenerator/static/js/runtime-main.06db69ba.js"
+  }
+]);
